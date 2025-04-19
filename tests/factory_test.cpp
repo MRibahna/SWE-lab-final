74361a1620ce6fb93f596ce23cbddf1012743baf
MRibahna <mahiamiti2001@gmail.com>
2025-04-19 14:50:25 +0600

    5th commit

--- tests/factory_test.cpp
+++ tests/factory_test.cpp
@@ -3,16 +3,16 @@
 #include "factory.h"
 
 TEST(FactoryTest, TestFunction1) {
-    // Add your test logic here
+    
     EXPECT_EQ(function1(), expected_value);
 }
 
 TEST(FactoryTest, TestFunction2) {
-    // Add your test logic here
+    
     EXPECT_TRUE(function2());
 }
 
 TEST(FactoryTest, TestFunction3) {
-    // Add your test logic here
+    
     EXPECT_NE(function3(), unexpected_value);
 }
